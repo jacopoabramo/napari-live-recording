f0e917f0c3f49e028335d582611136f894481c20
Jacopo Abramo <jacopo.abramo@gmail.com>
2021-10-25 15:37:23 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -25,12 +25,7 @@ except FileNotFoundError:
     long_description = DESCRIPTION
 
 about = {}
-if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
+about['__version__'] = VERSION
 
 requirements = []
 with open('requirements.txt') as f:
