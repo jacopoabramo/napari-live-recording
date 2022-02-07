1786a618d0e0f2dc462a7bd438ad11492893f949
jacopo <jacopo.abramo@gmail.com>
2022-02-07 20:55:01 +0100

    Renamed "tools" module to "common"

---
+++ napari_live_recording_rework/common/common.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+@dataclass
+class ROI:
+    """Dataclass for ROI settings.
+    """
+    offset_x: int = 0
+    offset_y: int = 0
+    height: int = 0
+    width: int = 0
+    ofs_x_step: int = 1
+    ofs_y_step: int = 1
+    width_step: int = 1
+    height_step: int = 1
