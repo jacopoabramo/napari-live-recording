5e012b83ea58aaa380b09159f8b96487f9f6c15f
Jacopo Abramo <jacopo.abramo@gmail.com>
2022-02-11 14:24:06 +0100

    Changed common device interface.

--- napari_live_recording_rework/devices/interface.py
+++ napari_live_recording_rework/devices/interface.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Union
+from typing import Any, Union
 from PyQt5.QtWidgets import QVBoxLayout
 import numpy as np
 from common import ROI
@@ -9,19 +9,20 @@ from widgets.widgets import (
     ComboBox,
     SpinBox, 
     DoubleSpinBox, 
-    LabeledSlider, 
-    LineEdit
+    LabeledSlider,
+    LineEdit,
+    WidgetEnum
 )
 
 ParameterType = Union[str, list[str], tuple[int, int, int], tuple[float, float, float]]
 
 class Camera(ABC):
     __availableWidgets = {
-        "combobox" : ComboBox,
-        "spinbox" : SpinBox,
-        "doublespinbox" : DoubleSpinBox,
-        "slider" : LabeledSlider,
-        "lineedit" : LineEdit
+        WidgetEnum.ComboBox : ComboBox,
+        WidgetEnum.SpinBox : SpinBox,
+        WidgetEnum.DoubleSpinBox : DoubleSpinBox,
+        WidgetEnum.LabeledSlider : LabeledSlider,
+        WidgetEnum.LineEdit : LineEdit
     }
     def __init__(self, name: str, deviceID: Union[str, int], paramDict: dict[str, LocalWidget], sensorShape: ROI) -> None:
         """Generic camera device. Each device lives in its own thread, and has a set of common widgets:
@@ -29,7 +30,7 @@ class Camera(ABC):
         - ROI handling widget;
         - Delete device button.
 
-        Constructor must be called AFTER calling the child class constructor to ensure that parameter widgets
+        Initializer must be called AFTER calling the child class initializer to ensure that parameter widgets
         can be accessed.
 
         Args:
@@ -42,26 +43,31 @@ class Camera(ABC):
         self.deviceID = deviceID
         self.layout = QVBoxLayout()
         self.ROIHandling = ROIHandling(sensorShape)
-        self.widgets = paramDict
-        for widget in self.widgets.values():
+        self.parameters = paramDict
+        for widget in self.parameters.values():
             self.layout.addLayout(widget.layout)
         self.layout.addLayout(self.ROIHandling.layout)
-
+        self.setupWidgetsForStartup()
+        self.connectSignals()
+    
+    def __del__(self) -> None:
+        self.closeDevice()
+    
     @abstractmethod
-    def initDevice(self, name: str, deviceID: Union[str, int]) -> None:
-        """Initializes the device.
+    def setupWidgetsForStartup(self) -> None:
+        """Initializes widgets for device startup.
         """
         pass
 
     @abstractmethod
-    def closeDevice(self) -> None:
-        """Closes the device if necessary.
+    def connectSignals(self) -> None:
+        """Connects widgets signals to device slots.
         """
         pass
 
     @abstractmethod
     def grabFrame(self) -> np.array:
-        """Returns the latest captured frame as a numpy array
+        """Returns the latest captured frame as a numpy array.
         """
         pass
 
@@ -71,17 +77,22 @@ class Camera(ABC):
         """
         pass
 
-    def addParameter(self, widgetType: str, name: str, unit: str, param: ParameterType, paramDict: dict[str, ParameterType], orientation="left") -> None:
+    def initDevice(self, name: str, deviceID: Union[str, int]) -> Any:
+        """Initializes the device.
+        """
+        pass
+
+    def closeDevice(self) -> None:
+        """Closes the device if necessary.
+        """
+        pass
+
+    def addParameter(self, widgetType: WidgetEnum, name: str, unit: str, param: ParameterType, paramDict: dict[str, ParameterType], orientation="left") -> None:
         """Adds a parameter in the form of a widget, exposing the respective signals to enable connections to user-defined slots.
         If the parameter already exists, it will not be added to the parameter list.
 
         Args:
-            - widgetType (str): type of widget created (case-independent), can be either:
-                - \"ComboBox\"
-                - \"SpinBox\"
-                - \"DoubleSpinBox\"
-                - \"LabeledSlider\"
-                - \"LineEdit\"
+            - widgetType (WidgetEnum): type of widget created.
             - name (str): name of the added parameter (i.e. \"Exposure time\")
                 - this will be the name shown in the GUI
             - unit (str): unit measure of the added parameter (i.e. \"ms\")
@@ -90,6 +101,6 @@ class Camera(ABC):
             - orientation (str, optional): orientation of the label for the parameter (can either be "left" or "right"). Default is "left".
         """
         if not name in self.parameters:
-            paramWidget = self.__availableWidgets[widgetType.lower()](param, name, unit, orientation)
+            paramWidget = self.__availableWidgets[widgetType](param, name, unit, orientation)
             paramDict[name] = paramWidget
 
