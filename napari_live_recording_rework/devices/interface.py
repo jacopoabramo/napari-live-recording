41ee03018de41521fe9fe10976e44cc67238e9af
Jacopo Abramo <jacopo.abramo@gmail.com>
2022-02-15 09:00:50 +0100

    Added live thread worker in camera interface.

--- napari_live_recording_rework/devices/interface.py
+++ napari_live_recording_rework/devices/interface.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 from typing import Any, Union
+from PyQt5.QtCore import QObject, Signal
 from PyQt5.QtWidgets import QPushButton, QVBoxLayout
+from napari.qt.threading import thread_worker
+from collections import deque
 import numpy as np
 from common import ROI
 from widgets.widgets import (
@@ -17,19 +20,22 @@ from widgets.widgets import (
 
 ParameterType = Union[str, list[str], tuple[int, int, int], tuple[float, float, float]]
 
-class Camera(ABC):
-    __availableWidgets = {
+class Camera(ABC, QObject):
+    availableWidgets = {
         WidgetEnum.ComboBox : ComboBox,
         WidgetEnum.SpinBox : SpinBox,
         WidgetEnum.DoubleSpinBox : DoubleSpinBox,
         WidgetEnum.LabeledSlider : LabeledSlider,
         WidgetEnum.LineEdit : LineEdit
     }
+
     def __init__(self, name: str, deviceID: Union[str, int], paramDict: dict[str, LocalWidget], sensorShape: ROI) -> None:
-        """Generic camera device. Each device lives in its own thread, and has a set of common widgets:
+        """Generic camera device. Each device has a set of common widgets:
 
         - ROI handling widget;
         - Delete device button.
+
+        Live and recording are handled using separate thread workers.
 
         Initializer must be called AFTER calling the child class initializer to ensure that parameter widgets
         can be accessed.
@@ -47,6 +53,8 @@ class Camera(ABC):
         self.recordHandling = RecordHandling()
         self.ROIHandling = ROIHandling(sensorShape)
         self.parameters = paramDict
+        self.recorded = Signal(np.array)
+        self.deleted = Signal(str)
 
         # layout order
         # 1) record handling widgets
@@ -60,9 +68,30 @@ class Camera(ABC):
         self.layout.addWidget(self.delete)
         self.setupWidgetsForStartup()
         self.connectSignals()
+        
+        # live recording
+        self.liveDeque = deque([], maxlen=10000)
+        self.liveWorker = self._acquireForever()
+        self.recordHandling.signals["liveRequested"].connect(self._handleLive)
+
+        # stack recording
+        # todo: add implementation
+
+        # deletion button clicked
+        self.delete.clicked.connect(lambda: self.deleted.emit(f"{self.name}:{self.deviceID}"))
     
     def __del__(self) -> None:
-        self.closeDevice()
+        # make sure that if live thread 
+        # is running, this quits it
+        self.recordHandling.signals["liveRequested"].emit(False)
+        self.close()
+        self.layout.deleteLater()
+    
+    @property
+    def latestLiveFrame(self) -> np.array:
+        """Pops a frame from the live deque, returning it.
+        """
+        return self.liveDeque.pop()
     
     @abstractmethod
     def setupWidgetsForStartup(self) -> None:
@@ -88,12 +117,7 @@ class Camera(ABC):
         """
         pass
 
-    def initDevice(self, name: str, deviceID: Union[str, int], *args, **kwargs) -> Any:
-        """Initializes the device if necessary.
-        """
-        pass
-
-    def closeDevice(self) -> None:
+    def close(self) -> None:
         """Closes the device if necessary.
         """
         pass
@@ -112,6 +136,37 @@ class Camera(ABC):
             - orientation (str, optional): orientation of the label for the parameter (can either be "left" or "right"). Default is "left".
         """
         if not name in self.parameters:
-            paramWidget = self.__availableWidgets[widgetType](param, name, unit, orientation)
+            paramWidget = self.availableWidgets[widgetType](param, name, unit, orientation)
             paramDict[name] = paramWidget
+    
+    @thread_worker(start_thread=False)
+    def _acquireForever(self):
+        """Private thread worker for live view acquisitions.
+        Captured images are stored in a ring buffer.
+        The worker is stopped via the quit() signal for a graceful exit.
+        """
+        while True:
+            img = self.grabFrame()
+            (self.liveDeque.append(img) if img is not None else None)
+            yield
+    
+    def _handleLive(self, isLive: bool) -> None:
+        """Private slot to start/stop live acquisition. Signals start() and quit() are called
+        when the thread needs to be started or stopped. When the thread is stopped, the live queue
+        is cleared.
 
+        Args:
+            isLive (bool): True if live acquisition is started, otherwise False.
+        """
+        if isLive:
+            self.liveWorker.start()
+        else:
+            self.liveWorker.quit()
+            self.liveDeque.clear()
+    
+    @thread_worker(start_thread=True)
+    def _acquireRecording(self, length: int) -> np.array:
+        """Private thread worker for recording acquisitions.
+        Captured images are stacked, stored on disk and then 
+        """
+        return np.stack([self.grabFrame() for _ in range(0, length)])
