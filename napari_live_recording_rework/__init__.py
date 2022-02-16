deffe23b6294697dd427ee1fcebe76916e2f31f5
Jacopo Abramo <jacopo.abramo@gmail.com>
2022-02-16 17:06:55 +0100

    Added more implementation.

--- napari_live_recording_rework/__init__.py
+++ napari_live_recording_rework/__init__.py
@@ -1,17 +1,21 @@
+import devices
 from napari import Viewer
 from napari_plugin_engine import napari_hook_implementation
 from napari_live_recording_rework.widgets import CameraSelection
 from napari_live_recording_rework.control import Controller
-from PyQt5.QtWidgets import QWidget
-from napari_live_recording_rework.devices.opencv import Camera
-# from napari_live_recording_rework.devices.ximea import Ximea
+from PyQt5.QtWidgets import QWidget, QVBoxLayout
 
 class napari_live_recording(QWidget):
     def __init__(self, napari_viewer : Viewer) -> None:
         super().__init__()
+        
+        self.mainLayout = QVBoxLayout(self)
         self.selectionWidget = CameraSelection()
+        self.selectionWidget.setAvailableCameras([devices.devicesDict.keys()])
         self.controller = Controller(napari_viewer)
 
+        self.mainLayout.addLayout(self.selectionWidget.layout())
+
 @napari_hook_implementation
-def napari_experimental_provide_dock_widget():
+def napari_live_recording_plugin():
     return [napari_live_recording]
