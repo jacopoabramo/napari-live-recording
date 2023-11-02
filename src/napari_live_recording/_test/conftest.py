b21eb17ec56741b16e541a418bd832748d971e91
Jacopo Abramo <jacopo.abramo@gmail.com>
2023-11-02 10:38:48 +0100

    Added clean-up fixture

--- src/napari_live_recording/_test/conftest.py
+++ src/napari_live_recording/_test/conftest.py
@@ -4,3 +4,16 @@ from napari_live_recording import NapariLiveRecording
 @pytest.fixture
 def recording_widget(make_napari_viewer):
     return NapariLiveRecording(make_napari_viewer())
+
+@pytest.fixture(autouse=True)
+def cleanup(recording_widget):
+    """ Performs exit cleanup for the test suite."""
+    # we yield control immediatly to the test;
+    yield
+
+    # after the test is done, we perform cleanup
+    widget : NapariLiveRecording = recording_widget
+    widget.on_close_callback()
+
+    assert len(list(widget.anchor.cameraWidgetGroups.keys())) == 0
+    assert len(list(widget.mainController.deviceControllers.keys())) == 0
