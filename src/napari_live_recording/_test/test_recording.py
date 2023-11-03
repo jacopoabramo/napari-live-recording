3063e1cf8f6fc17bc325e68781651407efdbcd15
Jacopo Abramo <jacopo.abramo@gmail.com>
2023-11-03 15:12:40 +0100

    Fixed UT

--- src/napari_live_recording/_test/test_recording.py
+++ src/napari_live_recording/_test/test_recording.py
@@ -1,32 +1,29 @@
 import logging
-from napari import Viewer
 from napari_live_recording import NapariLiveRecording
 
 logger = logging.getLogger(__name__)
 
-def test_mmcore_live_recording(recording_widget):
+def test_mmcore_live_recording(recording_widget, qtbot):
     widget : NapariLiveRecording = recording_widget
 
-    viewer : Viewer = widget.anchor.viewer
+    qtbot.addWidget(widget)
 
     widget.anchor.selectionWidget.camerasComboBox.combobox.setCurrentIndex(1) # MicroManager
     widget.anchor.selectionWidget.adapterComboBox.combobox.setCurrentIndex(8) # DemoCamera
     widget.anchor.selectionWidget.deviceComboBox.combobox.setCurrentIndex(0) # DCam
 
     widget.anchor.selectionWidget.addButton.click()
+    
+    events = [widget.anchor.recordingWidget.live.toggled, widget.anchor.liveTimer.timeout]
+
+    with qtbot.waitSignals(events, timeout=3000):
+        widget.anchor.recordingWidget.live.toggle()
+        assert widget.mainController.isLive == True
 
     widget.anchor.recordingWidget.live.toggle()
-
-    assert widget.mainController.isLive == True
-
-    widget.anchor.recordingWidget.live.toggle()
-
-    assert widget.mainController.isLive == False
+    assert widget.mainController.isLive == False    
 
     # the plugin when acquiring live produces a layer with the ID of the camera;
     # we can check if the layer is present or not
-
-    # TODO: apparently the layer is not created in the test environment;
-    # investigate why
-    # layer = viewer.layers["Live MyCamera:MicroManager:DemoCamera DCam"]
-    # logger.info(f"Layer: {layer.name}")
+    layer = widget.anchor.viewer.layers["Live MyCamera:MicroManager:DemoCamera DCam"]
+    assert layer is not None
