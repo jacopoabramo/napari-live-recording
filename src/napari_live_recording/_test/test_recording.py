f0672b4cf37fa59c5e52701a7bca0e2e79dec5fb
Jacopo Abramo <jacopo.abramo@gmail.com>
2023-11-29 18:48:24 +0100

    Further unit testing

--- src/napari_live_recording/_test/test_recording.py
+++ src/napari_live_recording/_test/test_recording.py
@@ -30,3 +30,13 @@ def test_mmcore_live_recording(recording_widget, qtbot):
     # we can check if the layer is present or not
     layer = widget.anchor.viewer.layers["Live MyCamera:MicroManager:DemoCamera DCam"]
     assert layer is not None
+
+def test_mmcore_stack_recording(recording_widget, qtbot):
+    widget : NapariLiveRecording = recording_widget
+    qtbot.addWidget(widget)
+    
+    widget.anchor.selectionWidget.camerasComboBox.combobox.setCurrentIndex(1) # MicroManager
+    widget.anchor.selectionWidget.adapterComboBox.combobox.setCurrentIndex(8) # DemoCamera
+    widget.anchor.selectionWidget.deviceComboBox.combobox.setCurrentIndex(0) # DCam
+
+    widget.anchor.selectionWidget.addButton.click()
